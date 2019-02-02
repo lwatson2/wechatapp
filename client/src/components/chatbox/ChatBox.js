440b415ef3392050419137fc9724fa8778f1f9ac
lwatson2 <watson.logan@yahoo.com>
2019-02-02 14:18:25 -0600

    Added material ui

--- client/src/components/chatbox/ChatBox.js
+++ client/src/components/chatbox/ChatBox.js
@@ -1,7 +1,25 @@
 import React, { Component } from "react";
+import "./ChatBox.css";
 
 export default class ChatBox extends Component {
+  state = {
+    chatValue: ""
+  };
+  handleChange = e => {
+    this.setState({ chatValue: e.target.value });
+  };
   render() {
-    return <div className="chatWindow" />;
+    return (
+      <div className="chatWindow">
+        <p>Chat window </p>
+        <div className="chatIput">
+          <input
+            type="text"
+            value={this.state.chatValue}
+            onChange={this.handleChange}
+          />
+        </div>
+      </div>
+    );
   }
 }
--- client/src/components/home/Home.js
+++ client/src/components/home/Home.js
@@ -1,9 +1,21 @@
 import React, { Component } from "react";
 import Axios from "axios";
 import NavBar from "../navBar/NavBar";
+import Drawer from "@material-ui/core/Drawer";
+import Hidden from "@material-ui/core/Hidden";
 import ChatBox from "../chatbox/ChatBox";
+import { withStyles } from "@material-ui/core/styles";
 
-export default class Home extends Component {
+const classes = () => ({
+  drawerPaper: {
+    width: 240,
+    backgroundColor: "blue"
+  }
+});
+class Home extends Component {
+  state = {
+    showSideBar: false
+  };
   componentDidMount() {
     const token = sessionStorage.getItem("token");
   }
@@ -12,16 +24,46 @@ export default class Home extends Component {
     Axios.get("/users/logout");
     this.props.history.push("/");
   };
+  handleSideBar = () => {
+    this.setState({ showSideBar: !this.state.showSideBar });
+  };
   render() {
+    const { classes } = this.props;
     return (
       <div>
         <div className="move">
           <p>Test</p>
           <button onClick={this.handleLogout}>Logout</button>
+          <button onClick={this.handleSideBar}>Show</button>
         </div>
-        <NavBar />
+        <Hidden smUp implementation="css">
+          <Drawer
+            variant="temporary"
+            open={this.state.showSideBar}
+            anchor="left"
+            onClose={this.handleSideBar}
+            classes={{
+              paper: classes.drawerPaper
+            }}
+          >
+            <NavBar />
+          </Drawer>
+        </Hidden>
+        <Hidden xsDown implementation="css">
+          <Drawer
+            variant="permanent"
+            open
+            className="drawer"
+            classes={{
+              paper: classes.drawerPaper
+            }}
+          >
+            <NavBar />
+          </Drawer>
+        </Hidden>
         <ChatBox />
       </div>
     );
   }
 }
+export default withStyles(classes)(Home);
