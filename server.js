17b94c376781a1474b8e6f8b5b9fe4dd52669015
lwatson2 <watson.logan@yahoo.com>
2019-02-12 11:17:36 -0600

    Added test for prod in server.js file

--- server.js
+++ server.js
@@ -40,10 +40,12 @@ app.use(passport.session());
 
 app.use("/users", require("./routes/users"));
 app.use("/groups", require("./routes/groups"));
-app.use(express.static(__dirname + "/client/build"));
-app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-});
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(__dirname + "/client/build"));
+  app.get("/*", function(req, res) {
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  });
+}
 
 //Socket setup
 const io = socket(server);
