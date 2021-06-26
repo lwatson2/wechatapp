15e328fb5bd3c0191feddec0e33e1b1fe565aee5
lwatson2 <watson.logan@yahoo.com>
2021-06-25 23:16:39 -0500

    re add dbConnect

--- client/pages/api/messages/[group].ts
+++ client/pages/api/messages/[group].ts
@@ -23,16 +23,6 @@ export default async function handler(
         res.status(400).json({ success: false });
       }
       break;
-      // case 'POST':
-      //   try {
-      //     const user = await User.create(
-      //       req.body
-      //     ) /* create a new model in the database */
-      //     res.status(201).json({ success: true, data: user })
-      //   } catch (error) {
-      //     res.status(400).json({ success: false })
-      //   }
-      break;
     default:
       res.status(400).json({ success: false });
       break;
